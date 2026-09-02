6c8d38caf503680c17f5a63a984f16d014370a6a
Jiří Prokop <jprokop.ofc@gmail.com>
2026-09-02 10:34:01 +0000

    feat: make backend json logs strictly one line long

--- federation-registry-backend-api/JavaScript/loggers.js
+++ federation-registry-backend-api/JavaScript/loggers.js
@@ -1,4 +1,5 @@
 var winston = require('winston');
+var {oneLineJson} = require('./logFormat');
 var logPath = __dirname + "/logs/logs.log";
 
 
@@ -35,7 +36,7 @@ const customLogger = (req,res,level,message,data)=>{
 const logger = winston.createLogger({
   format: winston.format.combine(
     winston.format.timestamp(),
-    winston.format.printf((info) => JSON.stringify(info).replace(/\\n|\r?\n/g, ' | '))
+    oneLineJson
   ),
   transports: [
     new winston.transports.Console({'timestamp':true}),
